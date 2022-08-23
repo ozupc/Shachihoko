f4b05a381e77c880a5742ff83ed0319ffb7b4005
ozupc <ozu.onepiece.312@gmail.com>
2022-08-23 17:03:02 +0900

    VectorXYZWコンポーネントを追加.

---
+++ Shachihoko/src/GLTF/VectorXYZWComponent.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Numerics;
+
+using Grasshopper;
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Parameters;
+
+using Rhino.Geometry;
+using Rhino.Runtime;
+
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.VertexTypes;
+using VERTEX = SharpGLTF.Geometry.VertexTypes.VertexPosition;
+using SharpGLTF.Materials;
+using SharpGLTF.Schema2;
+
+
+namespace Shachihoko
+{
+    public class VectorXYZWComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public VectorXYZWComponent()
+          : base("Vector XYZW", "Vector XYZW",
+            "Create a vector from [xyzw] components.",
+            "Shachihoko", ShachihokoMethod.Category["GLTF"])
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("X component", "X component", "Vector [x] component.", GH_ParamAccess.item, 0.0);
+            pManager.AddNumberParameter("Y component", "Y component", "Vector [y] component.", GH_ParamAccess.item, 0.0);
+            pManager.AddNumberParameter("Z component", "Z component", "Vector [z] component.", GH_ParamAccess.item, 0.0);
+            pManager.AddNumberParameter("W component", "W component", "Vector [w] component.", GH_ParamAccess.item, 0.0);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Vector", "Vector", "Vector construct.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //---<‰Šú‰»>---//
+            double x = 0.0;
+            double y = 0.0;
+            double z = 0.0;
+            double w = 0.0;
+
+            if (!DA.GetData(0, ref x)) return;
+            if (!DA.GetData(1, ref y)) return;
+            if (!DA.GetData(2, ref z)) return;
+            if (!DA.GetData(3, ref w)) return;
+
+            //---<Vector4‚Ìì¬>---//
+            Vector4 vector4 = new Vector4(((float)x), ((float)y), ((float)z), ((float)w));
+
+            //---<SetData>---//
+            DA.SetData(0, vector4);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// You can add image files to your project resources and access them like this:
+        /// return Resources.IconForThisComponent;
+        /// </summary>
+        protected override Bitmap Icon => null;
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("0FF628EE-BE08-4625-948D-640437F2FD1A\r\n");
+    }
+}
